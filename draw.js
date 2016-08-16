feacc6ee7dcf3ccd1cab48e5785ef97766fd33dd
Sanders DeNardi <sedenardi@gmail.com>
2016-08-16 00:13:00 -0400

    update gantt lib, start work on overlapping calculation

--- draw.js
+++ draw.js
@@ -16,13 +16,29 @@ var drawDay = function(date) {
     .map('Network')
     .uniq()
     .value();
-  var events = _.map(day, function(e) {
+  events = _.map(day, function(e) {
     e.startDate = new Date(e.Start);
     e.endDate = new Date(e.End);
     e.taskName = e.Network;
     e.status = e.LiveOrTape.indexOf('LIVE') === 0 ? 'LIVE' : 'TAPE';
+    e.keyFunction = function() {
+      return this.startDate + this.taskName + this.endDate + this.EventName;
+    };
     return e;
   });
+
+  var keyFunction = function(d) { return d.startDate + d.taskName + d.endDate + d.EventName; }
+
+  events = events.map(function(e1) {
+    e1.overlapping = events.filter(function(e2) {
+      var end1 = new Date(e1.endDate.getTime() - 1000);
+      var end2 = new Date(e2.endDate.getTime() - 1000);
+      return e1.startDate <= end2 && e2.startDate <= end1 &&
+        e1.taskName === e2.taskName;
+    }).length;
+    return e1;
+  });
+  console.log(events);
 
   var format = '%H:%M';
   var gantt = d3.gantt()
@@ -30,15 +46,16 @@ var drawDay = function(date) {
     .taskStatus(statuses)
     .tickFormat(format)
     .radius(5)
-    .baseClass('event');
+    .baseClass('event')
+    .keyFunction(keyFunction);
   gantt(events);
 
-  var selectors = d3.select('.chart')
-    .select('.gantt-chart')
-    .selectAll('rect');
-  selectors
-    .on('mouseenter', function(d) { console.log(d); })
-    .on('mouseleave', function(d) { console.log(d); });
+  // var selectors = d3.select('.chart')
+  //   .select('.gantt-chart')
+  //   .selectAll('rect');
+  // selectors
+  //   .on('mouseenter', function(d) { console.log(d); })
+  //   .on('mouseleave', function(d) { console.log(d); });
 };
 
-drawDay(Date.now());
+drawDay('2016-08-15');
